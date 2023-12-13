bc9ab53590026b1961e1d236f0c7b8e28cd0f734
larisapetroiu <iondragosc23@gmail.com>
2023-12-13 10:39:51 +0200

    Added database & restructered project

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyBwjhtOev-rp5qfnMLwQaUcy6Ayj5FxUl0",
+    authDomain: "nightowl-2b29e.firebaseapp.com",
+    databaseURL: "https://nightowl-2b29e-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "nightowl-2b29e",
+    storageBucket: "nightowl-2b29e.appspot.com",
+    messagingSenderId: "165209409454",
+    appId: "1:165209409454:web:5625132c64f0623f612a3f",
+    measurementId: "G-DW8D7HZJ16"
+  },
+  production: true
+};
+
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyBwjhtOev-rp5qfnMLwQaUcy6Ayj5FxUl0",
+    authDomain: "nightowl-2b29e.firebaseapp.com",
+    databaseURL: "https://nightowl-2b29e-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "nightowl-2b29e",
+    storageBucket: "nightowl-2b29e.appspot.com",
+    messagingSenderId: "165209409454",
+    appId: "1:165209409454:web:5625132c64f0623f612a3f",
+    measurementId: "G-DW8D7HZJ16"
+  },
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
